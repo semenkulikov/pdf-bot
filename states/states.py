509f327bc673ff6ebd2a804ec910895ca9daf053
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-03-07 18:09:40 +0300

    Added states for generating pdf

--- states/states.py
+++ states/states.py
@@ -3,7 +3,13 @@ from aiogram.fsm.state import StatesGroup, State
 
 class AdminPanel(StatesGroup):
     """ FSM для админ панели """
+    main = State()
     get_users = State()
+    pdf_admin_menu = State()
+    add_pdf_country = State()
+    edit_pdf_country = State()
+    add_pdf_service = State()
+    edit_pdf_service = State()
 
 class SubscribedState(StatesGroup):
     """ FSM для проверки подписки """
